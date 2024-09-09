fa4e00adcc330ab65b9e4ae7ff163cf7ca6dd951
IngoHermsen <you@example.com>
2024-09-09 16:45:15 +0200

    firebaseConfig in environment.development.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthComponent } from './auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/', component: AuthComponent},
+    { path: 'auth', component: AuthComponent},
+    { path: 'login', component: LoginComponent}
+];
