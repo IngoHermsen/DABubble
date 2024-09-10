91a27f32d416892bd699f06bd7cc1167db80403d
IngoHermsen <you@example.com>
2024-09-10 15:43:50 +0200

    generated main-components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthComponent } from './auth/auth.component';
+import { MainComponent } from './main-content/main-content.component';
 
 export const routes: Routes = [
-    { path: '/', component: AuthComponent},
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'auth', component: AuthComponent},
-    { path: 'login', component: LoginComponent}
+    { path: 'login', component: LoginComponent},
+    { path: 'home', component: MainComponent}
 ];
