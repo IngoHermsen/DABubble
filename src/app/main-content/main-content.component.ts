5926c4b108752b4a02b2abe518b12169bbaa9e4d
peet <teilzeitjobbewerber@gmx.de>
2024-09-14 13:34:21 +0200

    Refactor: dependencies of workspace working

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { ChannelComponent } from './channel/channel.component';
-import { ChannelSectionComponent } from './workspace/workspace.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 import { ThreadComponent } from './thread/thread.component';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ChannelComponent, ChannelSectionComponent, ThreadComponent],
+  imports: [ChannelComponent, WorkspaceComponent, ThreadComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  showWorkspaceMenu: boolean = false;
+
+  constructor() {
+    this.showWorkspaceMenu = true;
+    console.log(this.showWorkspaceMenu)
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
 
 }
