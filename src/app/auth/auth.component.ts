5949bb4662b322a2058493ce86beba99cc9f1ed1
peet <teilzeitjobbewerber@gmx.de>
2024-09-15 12:36:29 +0200

    Feat: auth animation logo moving slightly left and to end position.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
@@ -6,10 +7,20 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [LoginComponent, SignupComponent, ResetPasswordComponent],
+  imports: [LoginComponent, SignupComponent, ResetPasswordComponent, NgClass],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
+  constructor(){
+    this.delayAnimation()
+  }
 
+  testBoolean = false
+
+  delayAnimation(){
+    setTimeout(() => {
+      this.testBoolean = true
+    }, 2000);
+  }
 }
