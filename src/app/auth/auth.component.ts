17e8ed1f2da274a4f0e3424cfefca4a7c5bb0e83
peet <teilzeitjobbewerber@gmx.de>
2024-09-17 13:13:36 +0200

    Feat: setTimout implemented  in auth.ts

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
@@ -7,11 +7,21 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [LoginComponent, SignupComponent, ResetPasswordComponent, NgClass],
+  imports: [LoginComponent, SignupComponent, ResetPasswordComponent, NgClass, NgStyle],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
+ constructor(){
+  setTimeout(() => {
+    this.setColor = "black";
+    this.setSize = "50px";
+    this.setFontSize = "16px"
+  }, 2500);
+ } 
 
+ setColor = "";
+ setSize = "";
+ setFontSize = "";
 
 }
