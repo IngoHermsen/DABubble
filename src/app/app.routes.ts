3a1adcb837b357e50afce1d5c313ba382c69404a
peet <teilzeitjobbewerber@gmx.de>
2024-09-20 10:20:41 +0200

    Refactor: route changed " " to AuthComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { AuthComponent } from './auth/auth.component';
 import { MainComponent } from './main-content/main-content.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '', component: AuthComponent},
+    // { path: '', redirectTo: 'auth', pathMatch: 'full'},
     { path: 'auth', component: AuthComponent},
     { path: 'login', component: LoginComponent},
     { path: 'home', component: MainComponent}
