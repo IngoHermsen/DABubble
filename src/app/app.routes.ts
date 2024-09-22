f4cceade31b0e3289bbbc8736425162ff43d2712
peet <teilzeitjobbewerber@gmx.de>
2024-09-22 22:07:37 +0200

    Feat: signup route added in routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthComponent } from './auth/auth.component';
 import { MainComponent } from './main-content/main-content.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 export const routes: Routes = [
     {path: '', component: AuthComponent},
     // { path: '', redirectTo: 'auth', pathMatch: 'full'},
     { path: 'auth', component: AuthComponent},
     { path: 'login', component: LoginComponent},
-    { path: 'home', component: MainComponent}
+    { path: 'signup', component: SignupComponent}, 
+    { path: 'home', component: MainComponent}, 
 ];
