f394b80d84f39c2c688592d30eaabeca8741cfde
IngoHermsen <you@example.com>
2024-09-26 16:40:11 +0200

    Style: workspace entries dropBtn animation

--- src/app/core/directives/animate-toggle.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnimateToggleDirective } from './animate-toggle.directive';
-
-describe('AnimateToggleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AnimateToggleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/main-content/workspace/workspace.component.ts
+++ src/app/main-content/workspace/workspace.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
 })
 export class WorkspaceComponent {
+    showChannelEntries: boolean = true;
+    showDirectMsgEntries: boolean = true; 
+    channelToggleClicked: boolean = false; 
+    directMsgToggleClicked: boolean = false; 
 
+    toggleEntries(content: string) {
+        if(content == 'channels') {
+          this.channelToggleClicked = true;
+          this.showChannelEntries = !this.showChannelEntries;
+        } else {
+          this.directMsgToggleClicked = true;
+          this.showDirectMsgEntries = !this.showDirectMsgEntries;
+        }
+        console.log(content);
+    }
 }
