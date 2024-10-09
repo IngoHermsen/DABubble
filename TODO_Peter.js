fda65ab91702422346666de90e4718951b7cd968
peet <teilzeitjobbewerber@gmx.de>
2024-10-09 20:11:05 +0200

    Add: TODOs

--- TODO_Peter.js
+++ TODO_Peter.js
@@ -1,2 +1,17 @@
+
+
 // TODO Sign up Fenster Schreiben
-// TODO auth aus global raus und in auth component. 
+
+
+
+// TODO 7.10.2024
+// TODO Gedanken machen über Datenbankstruktur. 
+// TODO Welche Daten braucht ein User? 
+// TODO In welcher Form sollen diese Daten gespeichert werden? 
+
+// TODO 9.10.2024
+// TODO Weiter am mockup für login arbeiten
+// TODO Gedanken machen über Typographie Schriftgrößen responsive?
+// TODO Welche Breakpoints solen benutzt werden?
+// TODO Kann man die Responsivness irgendwie auslagern oder ist das Schwachsinn?
+
