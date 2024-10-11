d1103e1d959d80dcebadd3a5aca55c1d3df15b1d
IngoHermsen <you@example.com>
2024-10-11 15:31:27 +0200

    chore: removed props for User-Interface

--- src/app/core/interfaces/user.ts
+++ src/app/core/interfaces/user.ts
@@ -1,8 +1,5 @@
 export interface User {
     uid: string;
-    // firstName: string;
-    // lastName: string;
-    initials: string;
     email: string;
     displayName: string;
     emailVerified: boolean;
