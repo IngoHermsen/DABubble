30989b31309c43066975a9d383eafb789beef96c
peet <teilzeitjobbewerber@gmx.de>
2024-10-12 15:20:57 +0200

    @Ingo: Check mal die Frage in TODOs

--- TODO_Peter.js
+++ TODO_Peter.js
@@ -1,10 +1,10 @@
 
 
-// TODO Sign up Fenster Schreiben
 
 
 
 // TODO 9.10.2024
+// TODO Sign up Fenster Schreiben
 // TODO Weiter am mockup für login arbeiten
 // TODO Gedanken machen über Typographie Schriftgrößen responsive?
 // TODO Welche Breakpoints sollen benutzt werden?
@@ -14,3 +14,6 @@
 
 // TODO CSS / HTML für Direct Messages, mittleren Hauptbereich, mit ersten Pseudo-Einträgen und Pseudo-Usern
 // TODO Dialog "Profilbild-Erstellung" mit erster Storage-Anbindung in firebase für 'eigenes Profilbild" hochladne.
+
+
+//@ Ingo -- Check mal in styles.css den max-width-wrapper, und ob du den so in deiner Seite einbauen kannst?
