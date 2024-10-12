5c7d7670334b1d8ec7e142025925f22e96f36518
IngoHermsen <you@example.com>
2024-10-12 08:30:23 +0200

    Add: TODO

--- TODO_Peter.js
+++ TODO_Peter.js
@@ -14,3 +14,6 @@
 // TODO Gedanken machen über Typographie Schriftgrößen responsive?
 // TODO Welche Breakpoints sollen benutzt werden?
 // TODO Kann man die Responsivness irgendwie auslagern oder ist das Schwachsinn?
+
+// TODO CSS / HTML für Direct Messages, mittleren Hauptbereich, mit ersten Pseudo-Einträgen und Pseudo-Usern
+// TODO Dialog "Profilbild-Erstellung" mit erster Storage-Anbindung in firebase für 'eigenes Profilbild" hochladne.
