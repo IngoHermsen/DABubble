431a643fdc7623253eaa937c6c5bb407c150277c
peet <teilzeitjobbewerber@gmx.de>
2024-10-12 09:43:40 +0200

    TODO: added

--- TODO_Peter.js
+++ TODO_Peter.js
@@ -3,16 +3,14 @@
 // TODO Sign up Fenster Schreiben
 
 
-// TODO 7.10.2024
-// TODO Gedanken machen über Datenbankstruktur. 
-// TODO Welche Daten braucht ein User? 
-// TODO In welcher Form sollen diese Daten gespeichert werden? 
 
 // TODO 9.10.2024
 // TODO Weiter am mockup für login arbeiten
 // TODO Gedanken machen über Typographie Schriftgrößen responsive?
 // TODO Welche Breakpoints sollen benutzt werden?
 // TODO Kann man die Responsivness irgendwie auslagern oder ist das Schwachsinn?
+// TODO Google Auth auschecken
+
 
 // TODO CSS / HTML für Direct Messages, mittleren Hauptbereich, mit ersten Pseudo-Einträgen und Pseudo-Usern
 // TODO Dialog "Profilbild-Erstellung" mit erster Storage-Anbindung in firebase für 'eigenes Profilbild" hochladne.
