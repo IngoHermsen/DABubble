76ae60b31ed1f4b3ab63035cde349e60d869af68
IngoHermsen <you@example.com>
2024-10-14 14:17:18 +0200

    Feat: set 'avatar' route in routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { AuthComponent } from './auth/auth.component';
 import { MainComponent } from './main-content/main-content.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { EditAvatarComponent } from './auth/edit-avatar/edit-avatar.component';
 
 export const routes: Routes = [
     {path: '', component: AuthComponent},
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent},
     { path: 'signup', component: SignupComponent}, 
     { path: 'home', component: MainComponent}, 
+    { path: 'avatar', component: EditAvatarComponent}
 ];
