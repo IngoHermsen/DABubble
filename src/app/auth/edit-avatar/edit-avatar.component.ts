ec8ec194852e691960197293f3881d07cb432130
IngoHermsen <you@example.com>
2024-10-15 11:45:26 +0200

    Chore: Renamed avatar preset files

--- src/app/auth/edit-avatar/edit-avatar.component.ts
+++ src/app/auth/edit-avatar/edit-avatar.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FirebaseApp } from '@angular/fire/app';
 import { getStorage, ref, uploadBytes } from '@angular/fire/storage';
 
-  
+
 @Component({
   selector: 'app-edit-avatar',
   standalone: true,
@@ -11,29 +11,29 @@ import { getStorage, ref, uploadBytes } from '@angular/fire/storage';
   styleUrl: './edit-avatar.component.scss'
 })
 export class EditAvatarComponent {
- 
+
   fbStorage = getStorage();
   profileImagesRef = ref(this.fbStorage, 'profileimages/12345/profile-img');
 
   avatarImages = [
-    "avatar_female_1.png",
-    "avatar_female_2.png",
-    "avatar_male_1.png",
-    "avatar_male_2.png",
-    "avatar_male_3.png",
-    "avatar_male_4.png"
-  ] 
+    "avatar_1_female.png",
+    "avatar_2_male.png",
+    "avatar_3_male.png",
+    "avatar_4_male.png",
+    "avatar_5_female.png",
+    "avatar_6_male_3.png"
+  ]
 
   constructor(
     private firebaseApp: FirebaseApp
   ) {
-    
+
   }
 
   uploadImg(event: Event) {
     let inputEl = event.target as HTMLInputElement
-    if(inputEl?.files?.length) {
-      	uploadBytes(this.profileImagesRef, inputEl.files[0])
+    if (inputEl?.files?.length) {
+      uploadBytes(this.profileImagesRef, inputEl.files[0])
         .then((snapshot) => {
           console.log('upload completed', snapshot);
         })
