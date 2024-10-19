b3592b7c7595fdfe69cf909fab11ec33a89b5e62
peet <teilzeitjobbewerber@gmx.de>
2024-10-19 09:25:57 +0200

    Feat: redirection to post-animation works

---
+++ src/app/auth/auth-post-animation/auth-post-animation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-post-animation',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-post-animation.component.html',
+  styleUrl: './auth-post-animation.component.scss'
+})
+export class AuthPostAnimationComponent {
+
+}
