feb6e3b178388ccfb09ac5d4d94650beb5419a6c
peet <teilzeitjobbewerber@gmx.de>
2024-10-19 14:05:41 +0200

    Feat: Konto erstellen element added.

--- src/app/auth/auth-post-animation/auth-post-animation.component.ts
+++ src/app/auth/auth-post-animation/auth-post-animation.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { LoginComponent } from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
+import { ResetPasswordComponent } from '../reset-password/reset-password.component';
+import { RouterLink, RouterOutlet, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-auth-post-animation',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, NgClass, NgStyle, RouterLink],
   templateUrl: './auth-post-animation.component.html',
   styleUrl: './auth-post-animation.component.scss'
 })
