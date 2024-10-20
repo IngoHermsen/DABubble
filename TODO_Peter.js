9352551ad759a021009b98522d13b899aa11d0e1
peet <teilzeitjobbewerber@gmx.de>
2024-10-20 15:12:00 +0200

    TODO: added

--- TODO_Peter.js
+++ TODO_Peter.js
@@ -3,16 +3,24 @@
 
 
 
-// TODO 9.10.2024
+// TODO  --- 9.10.2024 --- 
+
 // TODO Sign up Fenster Schreiben
-// TODO Gedanken machen über Typographie Schriftgrößen responsive?
 // TODO Welche Breakpoints sollen benutzt werden?
-// TODO Kann man die Responsivness irgendwie auslagern oder ist das Schwachsinn?
 // TODO Google Auth auschecken
-
-
 // TODO CSS / HTML für Direct Messages, mittleren Hauptbereich, mit ersten Pseudo-Einträgen und Pseudo-Usern
 
 
 //@ Ingo -- Check mal in styles.css den max-width-wrapper, und ob du den so in deiner Seite einbauen kannst?
-//@ Ingo -- Lass uns kurz über die fonts Einbindungen sprechen. 
+
+
+// TODO  --- 20.10.2024 ---
+
+// @ Ingo -- Wollen wir die mixins alle unter mixins definieren oder auch thematisch?
+// Die Überlegung war den mixin loginSignupWindow() in auth.scss zu stecken. 
+// Habs aber erstmal in mixins.scss geschoben. Ist wahrscheinlich klarer von der Trennung.
+
+// @ Ingo -- So kann man mehrere commit messages in einem commit abfeuern, damit das in der commit history gut aussieht.
+ // git commit -m "Feat: Add this" -m "Doc: Updated README" --> Man benutzt einfach mehrmals -m
+ // check mal die p.sh kann für schnelleren git workflow benutzt werden. 
+ // Ich rufe die so auf ./p.sh ""
