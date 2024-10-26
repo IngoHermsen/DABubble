30feebaab6ca50623f5b358004c5d0ad99119a66
IngoHermsen <you@example.com>
2024-10-26 15:01:06 +0200

    Style: first channel posts styling

--- src/app/main-content/channel/channel.component.ts
+++ src/app/main-content/channel/channel.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './channel.component.scss'
 })
 export class ChannelComponent {
+  posts: any = [
+    {
+      userName: "Max Mustermann",
+      content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore distinctio, quisquam eligendi dolores error saepe nobis corporis. Quibusdam, nam iste. A possimus quo voluptatem laboriosam fugit totam vero voluptate asperiores."
+    },
+    {
+      userName: "Jochen Müller",
+      content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore distinctio, quisquam eligendi dolores error saepe nobis corporis. Quibusdam, nam iste. A possimus quo voluptatem laboriosam fugit totam vero voluptate asperiores."
+    },
+    {
+      userName: "Martina Musterfrau",
+      content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore distinctio, quisquam eligendi dolores error saepe nobis corporis. Quibusdam, nam iste. A possimus quo voluptatem laboriosam fugit totam vero voluptate asperiores."
 
+    },
+  ]
 }
