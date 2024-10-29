c745321856b37c0ca2ec9a1c4e664fe13dead22f
peet <teilzeitjobbewerber@gmx.de>
2024-10-29 16:10:21 +0100

    Rename: auth-post-animation --> auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AuthComponent } from './auth/auth-animation/auth-animation.component';
 import { MainComponent } from './main-content/main-content.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { EditAvatarComponent } from './auth/edit-avatar/edit-avatar.component';
-import { AuthPostAnimationComponent } from './auth/auth-post-animation/auth.component';
+import { AuthPostAnimationComponent } from './auth/auth/auth.component';
 
 export const routes: Routes = [
     {path: '', component: AuthComponent},
