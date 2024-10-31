8b3ed1feb43e8e7604909b9bff39985b654e8044
IngoHermsen <you@example.com>
2024-10-31 06:10:28 +0100

    Feat: created Post Component

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Post } from '../core/interfaces/post';
+
+@Component({
+  selector: 'app-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.scss'
+})
+export class PostComponent {
+  @Input() post: Post;
+}
