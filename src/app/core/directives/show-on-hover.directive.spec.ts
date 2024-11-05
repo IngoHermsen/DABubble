98553ba5c6cdd28e154f1c2b26a079a495957080
peet <teilzeitjobbewerber@gmx.de>
2024-11-05 12:50:13 +0100

    Refactor .spec files deleted

--- src/app/core/directives/show-on-hover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowOnHoverDirective } from './show-on-hover.directive';
-
-describe('ShowOnHoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowOnHoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
