a31d22b08d736de7753631938e83207b7efa7f8c
IngoHermsen <you@example.com>
2024-11-07 19:27:54 +0100

    Style: Post hover styles and menu

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Post } from '../core/interfaces/post';
 
 @Component({
@@ -9,13 +9,22 @@ import { Post } from '../core/interfaces/post';
   styleUrl: './post.component.scss'
 })
 export class PostComponent implements OnInit {
+  showPostMenu: boolean;
   @Input() index: number;
   @Input() post: Post;
   isOdd: boolean;
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.showPostMenu = true;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.showPostMenu = false;
+  }
+
   ngOnInit(): void {
-    this.isOdd = this.index % 2 !== 0 // Check if Index is Odd to get the correct style
-    console.log(this.isOdd)
+    this.isOdd = this.index % 2 !== 0 // Check if index is Odd to get the correct style
+    this.showPostMenu = false;
   }
 
 }
