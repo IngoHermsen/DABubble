edf7c523693333c781f7520806000399f84ad639
peet <teilzeitjobbewerber@gmx.de>
2024-11-07 11:15:00 +0100

    Refactor: renaming AuthPostAnimatinComponent --> AuthComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
-import { AuthComponent } from './auth/auth-animation/auth-animation.component';
+import { AuthAnimationComponent } from './auth/auth-animation/auth-animation.component';
 import { MainComponent } from './main-content/main-content.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { EditAvatarComponent } from './auth/edit-avatar/edit-avatar.component';
-import { AuthPostAnimationComponent } from './auth/auth/auth.component';
+import { AuthComponent } from './auth/auth/auth.component';
 
 export const routes: Routes = [
-    { path: '', component: AuthComponent },
+    { path: '', component: AuthAnimationComponent },
     {
-        path: 'main', component: AuthPostAnimationComponent,
+        path: 'main', component: AuthComponent,
         children:
             [
                 { path: 'login', component: LoginComponent },
