924115f44ab5dcdc9ea8ea72fe190b7fdecb28e7
IngoHermsen <you@example.com>
2024-11-07 18:36:37 +0100

    Feat: reactions span in post

--- src/app/core/interfaces/post.ts
+++ src/app/core/interfaces/post.ts
@@ -2,7 +2,7 @@ export interface Post {
     postId: string;
     creatorId: string;
     text: string;
-    reactions: []
+    reactions: any[]
     creationTime: string  // type 'string' for first test content, change later to type 'Date'
     isAnswer: boolean;
     imgPath?: string;
