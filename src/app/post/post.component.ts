601213b188909f5d343b91debe4803c4f3150847
IngoHermsen <you@example.com>
2024-11-09 11:18:20 +0100

    Feat: Emoji-Picker for Posts

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,17 @@
 import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Post } from '../core/interfaces/post';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [PickerComponent],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
 export class PostComponent implements OnInit {
   showPostMenu: boolean;
+  showEmojiMart: boolean; 
   @Input() index: number;
   @Input() post: Post;
   isOdd: boolean;
@@ -25,6 +27,11 @@ export class PostComponent implements OnInit {
   ngOnInit(): void {
     this.isOdd = this.index % 2 !== 0 // Check if index is Odd to get the correct style
     this.showPostMenu = false;
+    this.showEmojiMart = false;
+  }
+
+  addEmoji(clickEvent: Event) {
+    console.log('was here');
   }
 
 }
