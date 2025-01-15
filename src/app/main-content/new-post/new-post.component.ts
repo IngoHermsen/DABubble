7a0728ee04e75a4f4e88e82a1a7128d300fa904d
IngoHermsen <you@example.com>
2025-01-15 21:13:55 +0100

    Style: new post component

--- src/app/main-content/new-post/new-post.component.ts
+++ src/app/main-content/new-post/new-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MessageInputComponent } from '../message-input/message-input.component';
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [],
+  imports: [MessageInputComponent],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.scss'
 })
