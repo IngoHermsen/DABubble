1f647ec5b92cba9cb0e367e0ec637269b242b39f
peet <teilzeitjobbewerber@gmx.de>
2025-01-28 10:46:05 +0100

    Feat: imprint.html desktop finished

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ValidationService } from '../services/validation.service';
+
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
+
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
+
+
 export class ImprintComponent {
+  validation = inject(ValidationService);
 
 }
