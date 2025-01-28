5bfe3c1ccc34981669e8f4a947313dcc132f0f0d
peet <teilzeitjobbewerber@gmx.de>
2025-01-28 12:15:54 +0100

    Feat: privacy-container in privacy.html responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,26 +9,30 @@ import { NewPostComponent } from './main-content/new-post/new-post.component';
 import { DirectMessagesComponent } from './main-content/direct-messages/direct-messages.component';
 import { ChannelComponent } from './main-content/channel/channel.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 export const routes: Routes = [
     { path: '', component: AuthComponent },
-    { path: 'main', component: AuthPostAnimationComponent,
+    {
+        path: 'main', component: AuthPostAnimationComponent,
         children:
             [
                 { path: 'login', component: LoginComponent },
                 { path: 'signup', component: SignupComponent },
                 { path: 'imprint', component: ImprintComponent },
+                { path: 'privacy', component: PrivacyComponent}
             ]
     },
     { path: 'auth', component: AuthComponent },
-    { path: 'workspace', component: MainComponent, 
-        children: 
-        [
-            { path: 'channel', component: ChannelComponent},
-            { path: 'newPost', component: NewPostComponent},
-            { path: 'directMessages', component: DirectMessagesComponent}
-        ]      
+    {
+        path: 'workspace', component: MainComponent,
+        children:
+            [
+                { path: 'channel', component: ChannelComponent },
+                { path: 'newPost', component: NewPostComponent },
+                { path: 'directMessages', component: DirectMessagesComponent }
+            ]
     },
     { path: 'avatar', component: EditAvatarComponent },
-    
+
 ];
