5e39e64ba6fbaa89941dc97a016d42b795a7472f
IngoHermsen <you@example.com>
2025-01-28 18:31:12 +0100

    Feat: First mobile view setup in ViewService

--- src/app/core/services/view.service.ts
+++ src/app/core/services/view.service.ts
@@ -8,9 +8,11 @@ export class ViewService {
   private router = inject(Router)
   showThreadSection: boolean = false;
   showDialog: boolean = false;
-  
+  mobileView: boolean;  
 
   constructor() { 
+    this.mobileView = window.innerWidth <= 880;
+    console.log(`windows.innerwidth:${window.innerWidth}, mobileView = ${this.mobileView}`)
   }
 
   navigate(route: string) {
