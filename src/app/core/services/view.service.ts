1212064d642904c7c16f39ad16b66995d5d8beae
IngoHermsen <you@example.com>
2025-02-04 17:09:26 +0100

    Refactor: Mobile view in workspace header

--- src/app/core/services/view.service.ts
+++ src/app/core/services/view.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, Type } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViewService {
+  private router = inject(Router);
+  showThreadSection: boolean = false;
+  showDialog: boolean = false;
 
-  constructor() { }
+  // mobile options
+  mobileView: boolean;
+  showLogo: boolean = true; 
+  
+
+  constructor() { 
+    this.mobileView = window.innerWidth <= 880;
+    console.log(`windows.innerwidth:${window.innerWidth}, mobileView = ${this.mobileView}`)
+  }
+
+  wsNavigate(route: string) {
+    console.log(route);
+    this.showLogo = route == 'workspace' ? true : false;
+    this.router.navigate([route]);
+
+  }
 }
