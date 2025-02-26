d38d091804d23779f3cbd105cde0fa974228ec51
peet <teilzeitjobbewerber@gmx.de>
2025-02-26 02:02:02 +0100

    Feat: added property to User interface

--- src/app/core/interfaces/user.ts
+++ src/app/core/interfaces/user.ts
@@ -4,4 +4,5 @@ export interface User {
     directMessages: any[]; 
     avatarPath: string;
     isOnline: boolean;
+    photoURL: string;
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -136,6 +136,7 @@ export class AuthService {
       directMessages: [],
       avatarPath: "",
       isOnline: false,
+      photoURL: "",
     };
   }
 
